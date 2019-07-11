39996b1e8a7ef85b310767ab1849d0548705292a
gwang74 <jhell@qq.com>
2019-07-11 15:56:37 +0800

    update

--- library/src/test/java/com/android/jtwallet/ExampleUnitTest.java
+++ library/src/test/java/com/android/jtwallet/ExampleUnitTest.java
@@ -27,21 +27,8 @@ public class ExampleUnitTest {
         JtKeyPair jtKeyPair = new JtKeyPair("jBvrdYc6G437hipoCiEpTwrWSRBS2ahXN6", "snBPyRRpE56ea4QGCpTMVTQWoirT2");
         try {
             KeyStoreFile keyStoreFile = KeyStore.createLight("Key123456", jtKeyPair);
-            System.out.println("address:" + jtKeyPair.getAddress());
-            System.out.println("PrivateKey:" + jtKeyPair.getPrivateKey());
-            System.out.println("cipher:" + keyStoreFile.getCrypto().getCipher());
-            System.out.println("ciphertext:" + keyStoreFile.getCrypto().getCiphertext());
-            System.out.println("cipherparams.iv:" + keyStoreFile.getCrypto().getCipherparams().getIv());
-            System.out.println("kdf:" + keyStoreFile.getCrypto().getKdf());
-            KeyStoreFile.ScryptKdfParams scryptKdfParams = (KeyStoreFile.ScryptKdfParams) (keyStoreFile.getCrypto().getKdfparams());
-            System.out.println("kdfparams.dklen:" + scryptKdfParams.getDklen());
-            System.out.println("kdfparams.n:" + scryptKdfParams.getN());
-            System.out.println("kdfparams.p:" + scryptKdfParams.getP());
-            System.out.println("kdfparams.r:" + scryptKdfParams.getR());
-            System.out.println("kdfparams.salt:" + scryptKdfParams.getSalt());
-            System.out.println("mac:" + keyStoreFile.getCrypto().getMac());
-            System.out.println("id:" + keyStoreFile.getId());
-            System.out.println("v:" + keyStoreFile.getVersion());
+            System.out.println(keyStoreFile.toString());
+            System.out.println("===============decrypt============");
             JtKeyPair decryptEthECKeyPair = KeyStore.decrypt("Key123456", keyStoreFile);
             System.out.println("address:" + decryptEthECKeyPair.getAddress());
             System.out.println("PrivateKey:" + decryptEthECKeyPair.getPrivateKey());
